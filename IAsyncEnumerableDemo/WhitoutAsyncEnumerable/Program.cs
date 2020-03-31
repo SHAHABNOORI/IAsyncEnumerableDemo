107a51a0d3c865bb0d002de29984ff684753f9b3
shahabnoori <noorigoodarzi@gmail.com>
2020-04-01 01:38:22 +0430

    Inital

---
+++ IAsyncEnumerableDemo/WhitoutAsyncEnumerable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WithoutAsyncEnumerable
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            foreach (var dataPoint in await FetchIotData())
+            {
+                Console.WriteLine(dataPoint);
+            }
+
+            Console.ReadLine();
+        }
+
+        static async Task<IEnumerable<int>> FetchIotData()
+        {
+            List<int> dataPoints = new List<int>();
+            for (int i = 1; i <= 10; i++)
+            {
+                await Task.Delay(1000);//Simulate waiting for data to come through. 
+                dataPoints.Add(i);
+            }
+            return dataPoints;
+        }
+    }
+}
